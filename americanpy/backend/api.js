e058a014f06c468e9a4d8e8ea463a4bacff0a32b
Gilheim <Guy.vernon@mac.com>
2023-08-07 15:38:06 +0100

    all backend files initialised

--- americanpy/backend/api.js
+++ americanpy/backend/api.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const logRoutes = require('./middleware/logger');
-
+const userRouter = require('./routes/user')
+const countryRouter = require('./routes/country')
+const rewardRouter = require('./routes/reward')
+const memoryRouter = require('./routes/memory')
 
 const api = express();
 
@@ -13,6 +16,11 @@ api.use(logRoutes);
 api.get('/', (req,res) => {
     res.status(200).send("Welcome to the PythonlyFans App")
 })
+
+api.use('/users', userRouter)
+api.use('/country', countryRouter)
+api.use('/memory', memoryRouter)
+api.use('./reward', rewardRouter)
 
 
 module.exports = api;
---
+++ americanpy/backend/controllers/rewardController.js
@@ -0,0 +1,8 @@
+const Reward = require('../models/Reward')
+
+class RewardController {
+
+}
+
+module.exports = RewardController
+
