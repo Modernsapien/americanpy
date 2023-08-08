7d08d0b9061b442c3db1e84bef118179726b4ef7
kieranscutt <kieranscutt142@gmail.com>
2023-08-08 14:18:08 +0100

    Country routes, models and controllers

--- americanpy/backend/routes/country.js
+++ americanpy/backend/routes/country.js
@@ -5,6 +5,8 @@ const countryController = require('../controllers/countryController')
 const countryRouter = Router()
 
 
-
+countryRouter.get('/', countryController.getAllCountry)
+countryRouter.get('/:id', countryController.getCountryByID)
+countryRouter.patch("/:id", countryController.updateCountry)
 
 module.exports = countryRouter
