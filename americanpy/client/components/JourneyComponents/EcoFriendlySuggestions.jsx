a4bb4caf3d68967321ab311bf3ab33736e9784a4
Modernsapien <danielscott907@yahoo.com>
2023-08-11 17:09:33 +0100

    trying to get aiworking on journey page

--- americanpy/client/components/JourneyComponents/EcoFriendlySuggestions.jsx
+++ americanpy/client/components/JourneyComponents/EcoFriendlySuggestions.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import OpenAI from 'openai-api';
 
 const EcoFriendlySuggestions = () => {
   const [startDestination, setStartDestination] = useState('');
   const [endDestination, setEndDestination] = useState('');
   const [ecoFriendlySuggestions, setEcoFriendlySuggestions] = useState([]);
+  const OPENAI_API_KEY = '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';
 
   const handleJourneySubmit = async (event) => {
     event.preventDefault();
@@ -19,37 +20,31 @@ const EcoFriendlySuggestions = () => {
   };
 
   const generateEcoFriendlySuggestions = async (start, end) => {
-    const OPENAI_API_KEY = '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';
-    const openai = new OpenAI(OPENAI_API_KEY);
+    const openai = new OpenAI({ key: OPENAI_API_KEY });
 
     try {
-      const prompt = `User: I want to make a sustainable travel choice for my journey. Please recommend three specific ways to travel considering the environmental issues of global warming and the process of burning. My journey involves the options of riding a bike, taking public transportation (bus, train, metro), and driving.
-
-      AI: Based on your preferences and the environmental concerns you mentioned, here are three recommended ways to make your journey more sustainable:
+      const prompt = `Given the start destination "${start}" and end destination "${end}", provide detailed recommendations for eco-friendly travel options with low carbon emissions. For each mode of travel (walking, biking, public transportation, carpooling), please include the following information:
+      1. Explain the specific conditions under which this mode of travel is more or less applicable.
+      2. Compare the typical travel times between this mode of travel and others.
+      3. Provide recommendations for activities during cruising on the river while using this mode of travel.
       
-      1. Riding a Bike: Biking is an eco-friendly option that produces zero emissions and helps reduce your carbon footprint. It's a great choice for short distances and promotes a healthier lifestyle.
+      Additionally, discuss how each mode of travel can be made more sustainable and highlight any extra environmental benefits.
       
-      2. Taking Public Transportation: Opting for public transportation, such as buses, trains, or the metro, can significantly lower carbon emissions compared to private driving. It also helps alleviate traffic congestion and supports public infrastructure.
-      
-      3. Carpooling: If driving is necessary, consider carpooling with others to share the environmental impact. Carpooling reduces the number of vehicles on the road and decreases emissions per person, making your journey more sustainable.
-      
-      These recommendations take into account the environmental issues you mentioned and provide practical options for a greener travel experience. Have a safe and eco-friendly journey!`;
-      
-        
+      Your insights will be used to create research papers focused on practical options for maximizing carbon savings while traveling between these destinations.`;
 
       const gptResponse = await openai.complete({
-  engine: 'davinci', 
-  prompt: prompt,
-  maxTokens: 150,  // Adjust the token limit based on your needs
-});
+        engine: 'davinci',
+        prompt: prompt,
+        maxTokens: 150, // Adjust the token limit based on your needs
+      });
 
-
-      const suggestionText = gptResponse.data.choices[0].text;
+      const suggestionText = gptResponse.choices[0].text;
       return [suggestionText];
     } catch (error) {
       throw error;
     }
   };
+
   return (
     <div className="eco-friendly-suggestions">
       <h2>Eco-Friendly Travel Suggestions:</h2>
@@ -82,11 +77,3 @@ const EcoFriendlySuggestions = () => {
 };
 
 export default EcoFriendlySuggestions;
-
-
-
-
-
-
-
-
