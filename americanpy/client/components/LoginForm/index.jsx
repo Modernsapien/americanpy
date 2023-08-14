9e86cc572ac17b813924668b1bbdcfcf39521130
Abdul Salim <100780536+salim1704@users.noreply.github.com>
2023-08-14 01:48:57 +0100

    changes to login/register

--- americanpy/client/components/LoginForm/index.jsx
+++ americanpy/client/components/LoginForm/index.jsx
@@ -1,59 +1,67 @@
-import React from 'react'
-import { useState } from 'react'
-import 'americanpy/client/pages/LoginPage/LoginPage.css'
-import { useNavigate } from 'react-router'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { useAuth } from '../../contexts';
+import styles from '../../pages/LoginPage/LoginPage';
 
-import { useAuth } from '../../contexts'
-  
 export default function LoginForm(props) {
-    const navigate = useNavigate()
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-  
-    const {token, setToken } = useAuth()
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault()
-      const options = {
-        method: 'POST',
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password
-        })
-      }
-      const resp = await fetch('https://travel-wise-api.onrender.com/users/login', options)
-      const data = await resp.json()
-      if (resp.ok) {
-        localStorage.setItem("token", data.token);
-        setToken(data.token)
-        navigate('/')
-      } else {
-        console.log(data)
-        alert('Incorrect username or password')
-      }
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { token, setToken } = useAuth();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const options = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    };
+    const resp = await fetch('https://travel-wise-api.onrender.com/users/login', options);
+    const data = await resp.json();
+    if (resp.ok) {
+      localStorage.setItem('token', data.token);
+      setToken(data.token);
+      navigate('/');
+    } else {
+      console.log(data);
+      alert('Incorrect username or password');
     }
-  
-    return (
-    <div className={styles.copntainerWrapper}>
-    <div className="container-wrapper">
-      <div className="top">
-        <div className="logo"></div>
+  };
+
+  return (
+    <div className={styles.containerWrapper}>
+      <div className="auth-form-container" data-testid="formContainer">
+        <h2 className="form-title">Login</h2>
+
+        <form className="auth-form-login" onSubmit={handleSubmit} role="loginForm" data-testid="loginForm">
+          <input
+            placeholder="Username"
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button data-testid="logInLogIn" type="submit" className="signinBtn">
+            Log in
+          </button>
+        </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('registerForm')}>
+          Don't have an account? Register here.
+        </button>
       </div>
-      <div className='signin-form' data-testid = "formContainer">
-        <h2 className='form-title'>Login</h2>
-  
-      <form className='auth-form-login' onSubmit={handleSubmit} role='loginForm' data-testid="loginForm">
-        <input placeholder='username' type="username" id='username' value={username} onChange={(e) => setUsername(e.target.value)} />
-        <input placeholder='password' type="password" id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button data-testid = "logInLogIn" type='submit'>Log in</button>
-      </form>
-       <button className="inline-p" onClick={() => props.onFormSwitch('registerForm')}>Don't have an account? Register here.</button>
-      </div>
-      </div>
-      </div>
-    )
-  }
+    </div>
+  );
+}
--- americanpy/client/components/RegisterForm/index.jsx
+++ americanpy/client/components/RegisterForm/index.jsx
@@ -1,49 +1,102 @@
-import React, {useState} from "react"
-import '../../pages/LoginPage/LoginPage.css'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import styles from '../../pages/LoginPage/LoginPage.css'; 
+import { useAuth } from '../../contexts';
 
 export default function RegisterForm(props) {
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('')
-    const [password, setPassword] = useState('');
-    const [username, setUsername] = useState('');
+  const navigate = useNavigate();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const { token, setToken } = useAuth();
 
-   const handleSubmit = async (e) => {
-      e.preventDefault()
-      const options = {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username,
-          password: password,
-          firstName: firstName,
-          lastName: lastName
-        }),
-      }
-      const resp = await fetch('https://travel-wise-api.onrender.com/users/register', options)
-      const data = await resp.json()
-      if (resp.ok) {
-        console.log(data)
-        window.location.href = '/login'
-      } else {
-        console.log(data)
-        alert('Username already exists')
-      }
-   }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const options = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+      }),
+    };
+    const resp = await fetch('https://travel-wise-api.onrender.com/users/register', options);
+    const data = await resp.json();
+    if (resp.ok) {
+      console.log(data);
+      window.location.href = '/login';
+    } else {
+      console.log(data);
+      alert('Username already exists');
+    }
+  };
 
-    return (
-        <div className="auth-form-container" data-testid = "formContainer">
-            <h2 className="form-title">Register</h2>
+  return (
+    <div className={styles.containerWrapper}>
+      <div className="auth-form-container" data-testid="formContainer">
+        <h2 className="form-title">Register</h2>
         <form className="auth-form-register" onSubmit={handleSubmit} data-testid="loginForm">
-          <input placeholder="First Name" type="text" id='firstName' data-testid='firstName' value={firstName} required onChange={(e) => setFirstName(e.target.value)} />
-          <input placeholder="Last Name" type="text" id='lastName' data-testid='lastName' value={lastName} required onChange={(e) => setLastName(e.target.value)} />
-          <input placeholder="Username" type="username" id="username" data-testid="username" value={username} required onChange={(e) => setUsername(e.target.value)} />
-          <input placeholder="Password" type="password" id='password' data-testid='password' value={password} required onChange={(e) => setPassword(e.target.value)} />
-          <button type='submit'>Register</button>
+          <input
+            placeholder="Username"
+            type="text"
+            id="username"
+            data-testid="username"
+            value={username}
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            id="password"
+            data-testid="password"
+            value={password}
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            placeholder="First Name"
+            type="text"
+            id="firstName"
+            data-testid="firstName"
+            value={firstName}
+            required
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            placeholder="Last Name"
+            type="text"
+            id="lastName"
+            data-testid="lastName"
+            value={lastName}
+            required
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <input
+            placeholder="Email"
+            type="text"
+            id="email"
+            data-testid="email"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button type="submit" className="signinBtn">
+            Register
+          </button>
         </form>
-         <button className="link-btn" onClick={() => props.onFormSwitch('LoginForm')}>Have an account? Log in here.</button>
-        </div> 
-    )
+        <button className="link-btn" onClick={() => props.onFormSwitch('LoginForm')}>
+          Have an account? Log in here.
+        </button>
+      </div>
+    </div>
+  );
 }
